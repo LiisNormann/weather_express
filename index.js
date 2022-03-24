8a1ff93c25b94e446759008ae26d05709285bf2b
Liis Normann <liis.normann@gmail.com>
2022-03-24 12:05:11 +0000

    Created API error handling

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fetch = require('node-fetch');
-
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended:true}));
+const url = require("url");
+const {response} = require("express");
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -13,8 +14,7 @@ const key = '7ce4e3e8bcb9f4b0bc884650a2209a15';
 
 const getWeatherDataPromise = (url) => {
     return new Promise((resolve, reject) => {
-        fetch(url)
-            .then(response => {
+        fetch(url).then(response => {
                 return response.json()
             })
             .then(data => {
@@ -24,7 +24,8 @@ const getWeatherDataPromise = (url) => {
                 let result = {
                     description: description,
                     city: city,
-                    temp: temp
+                    temp: temp,
+                    error: null
                 }
                 resolve(result)
             })
@@ -34,21 +35,22 @@ const getWeatherDataPromise = (url) => {
     })
 }
 
-app.all('/', function(req, res) {
+app.all('/', function (req,res){
     let city
-    if(req.method == 'GET') {
-        city = 'Tartu';
+    if(req.method == 'GET'){
+        city = 'Tartu'
     }
-
-    if(req.method == 'POST') {
-        city = req.body.cityname;
+    if(req.method == 'POST'){
+        city = req.body.cityname
     }
-
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
     getWeatherDataPromise(url)
         .then(data => {
-        res.render('index', data)
-    })
+            res.render('index', data)
+        })
+        .catch(error =>{
+            res.render( 'index', {error: 'Problem with getting data, try again!!'})
+        })
 })
 
 
